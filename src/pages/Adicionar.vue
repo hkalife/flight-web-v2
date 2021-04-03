e4123bbd87008f8867a4c0d9625bdf9bcf17fbda
hkalife <henriquekalife@gmail.com>
2021-04-03 17:11:38 -0300

    feat(airport): add and delete

--- src/pages/Adicionar.vue
+++ src/pages/Adicionar.vue
@@ -1,11 +1,94 @@
 <template>
   <q-page class="flex flex-center">
-    <h4>Adicionar</h4>
+    <q-card square bordered class="q-pa-xl shadow-1 airportList">
+      <h4 class="q-ml-md">Adicionar</h4>
+
+      <div class="q-pa-md">
+        <div class="row col-md-10 offset-md-2 wholeAirports">
+          <div class="col-md-2 mapAirports">
+            <q-input outlined v-model="nome" label="Nome" />
+          </div>
+          <div class="col-md-2 mapAirports">
+            <q-input outlined v-model="icao" label="ICAO" />
+          </div>
+          <div class="col-md-2 mapAirports">
+            <q-input outlined v-model="iata" label="IATA" />
+          </div>
+          <div class="col-md-2 mapAirports">
+            <q-input outlined v-model="cidade" label="Cidade" />
+          </div>
+          <div class="col-md-2 mapAirports">
+            <q-input outlined v-model="pais" label="País" />
+          </div>
+        </div>
+      </div>
+
+      <q-card-section>
+        <q-btn color="primary" class="q-mt-lg" label="Cadastrar" @click="validarEEnviar()" />
+      </q-card-section>
+    </q-card>
   </q-page>
 </template>
 
 <script>
 export default {
-  name: 'PageIndex'
+  name: 'PageIndex',
+  data () {
+    return {
+      nome: '',
+      icao: '',
+      iata: '',
+      cidade: '',
+      pais: '',
+      apiUrl: 'http://localhost:3000/aeroportos'
+    }
+  },
+  methods: {
+    validarEEnviar () {
+      if (this.nome === '' || this.icao === '' || this.iata === '' || this.cidade === '' || this.pais === '') {
+        this.$q.notify({
+          type: 'create-user-error',
+          icon: 'error',
+          progress: true,
+          color: 'red',
+          textColor: 'white',
+          message: 'Todos os campos precisam estar preenchidos!'
+        })
+      } else {
+        const payloadCreateAirport = {
+          nome: this.nome,
+          icao: this.icao,
+          iata: this.iata,
+          cidade: this.cidade,
+          pais: this.pais
+        }
+
+        this.$axios.post(this.apiUrl, payloadCreateAirport).then(() => {
+          this.$q.notify({
+            type: 'create-user-success',
+            icon: 'check',
+            progress: true,
+            color: 'primary',
+            textColor: 'white',
+            message: 'Aeroporto criado com sucesso!'
+          })
+          this.$router.push({ path: '/' })
+        }).catch(() => {
+          this.$q.notify({
+            type: 'negative',
+            icon: 'error',
+            message: 'Ocorreu um erro. Tente novamente.'
+          })
+        })
+      }
+    }
+  },
+  mounted () {
+    this.nome = ''
+    this.icao = ''
+    this.iata = ''
+    this.cidade = ''
+    this.pais = ''
+  }
 }
 </script>
--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -1,11 +1,89 @@
 <template>
   <q-page class="flex flex-center">
-    <h4>Listar</h4>
+    <q-card square bordered class="q-pa-xl shadow-1 airportList">
+      <h4 class="q-ml-md">Listar</h4>
+
+      <div class="q-pa-md">
+        <div class="row col-md-10 offset-md-2 wholeAirports">
+          <div class="col-md-2 mapAirports" v-for="airport in currentAirports" :key="airport.id">
+            <q-card class="airportCard">
+              <q-card-section>
+                <div class="text-h6 text-center">{{ airport.nome }}</div>
+              </q-card-section>
+
+              <q-separator />
+
+              <q-card-section>
+                <div class="text-p text-center">ICAO: {{ airport.ICAO }}</div>
+                <div class="text-p text-center">IATA: {{ airport.iata }}</div>
+                <div class="text-p text-center">Cidade: {{ airport.cidade }}</div>
+                <div class="text-p text-center">País: {{ airport.pais }}</div>
+              </q-card-section>
+
+              <q-separator />
+
+              <q-card-section>
+                <q-btn color="red" glossy label="Excluir" @click="deleteAirport(airport.id)" />
+                <q-btn color="white" text-color="black" label="Editar" />
+              </q-card-section>
+            </q-card>
+          </div>
+        </div>
+      </div>
+    </q-card>
   </q-page>
 </template>
 
 <script>
 export default {
-  name: 'PageIndex'
+  name: 'PageIndex',
+  data () {
+    return {
+      currentAirports: [],
+      apiUrl: 'http://localhost:3000/aeroportos'
+    }
+  },
+  methods: {
+    fetchAirports () {
+      this.$axios.get(this.apiUrl).then(response => {
+        this.currentAirports = response.data
+        console.log(this.currentAirports)
+      }).catch(err => {
+        console.log(err)
+      })
+    },
+    deleteAirport (id) {
+      const deleteUrl = `${this.apiUrl}/${id}`
+      this.$axios.delete(deleteUrl).then(() => {
+        this.fetchAirports()
+      }).catch(err => {
+        console.log(err)
+      })
+    }
+  },
+  mounted () {
+    this.fetchAirports()
+  }
 }
 </script>
+
+<style>
+.airportList {
+  width: 80%;
+}
+
+.airportCard {
+  width: 200px;
+}
+
+.mapAirports {
+  margin: 10px;
+  text-align: -webkit-center;
+}
+
+.wholeAirports {
+  display: flex;
+  justify-content: center;
+  justify-content: space-evenly;
+}
+</style>
